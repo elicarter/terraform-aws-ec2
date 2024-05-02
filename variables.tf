0618ce2d82449dde6dcf313c3baa1fab357bc5e7
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-02 09:48:10 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "ami_id" {
   description = "The AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0c55b159cbfafe1f0"  # Default to Amazon Linux 2 AMI
+  default     = # Default to Amazon Linux 2 AMI
 }
 
 variable "instance_type" {
