461251e466ab2259c9742b2d9745ef5dde9ddeb7
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-02 05:58:06 -0600

    adding ec2 module config files

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# define EC2 instance resources
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  count         = "1"
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default = "us-west-2"
+}
+
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0"  # Default to Amazon Linux 2 AMI
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance"
+  type        = string
+  default     = "t2.micro"  # Default to t2.micro instance type
+}
