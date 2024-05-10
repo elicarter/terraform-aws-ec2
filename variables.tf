7deff9cbafe20d520f22c4d050eced38f4e48a90
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-10 06:34:18 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,12 +4,6 @@ variable "aws_region" {
   default = "us-west-2"
 }
 
-variable "ami_id" {
-  description = "The AMI ID for the EC2 instance"
-  type        = string
-  default     = "ami-0c55b159cbfafe1f0"
-}
-
 variable "instance_type" {
   description = "The instance type for the EC2 instance"
   type        = string
